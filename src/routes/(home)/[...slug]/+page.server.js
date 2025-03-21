56838143f8572266b0a613bf151d8dc121bcb4f5
matteopiatti <github@piatti.li>
2025-03-21 16:24:33 +0100

    add update functionality

--- src/routes/(home)/[...slug]/+page.server.js
+++ src/routes/(home)/[...slug]/+page.server.js
@@ -1,6 +1,7 @@
 import { Pages } from "$lib";
 
 export const load = async ({ locals, params }) => {
+  // this comment is to test update functionality
   return {
     page: await Pages.SHOW(params.slug),
   };
