cd7664957b5335af5d42d96096747975e81d9686
matteopiatti <github@piatti.li>
2025-03-21 16:36:40 +0100

    remove update comment

--- src/routes/(home)/[...slug]/+page.server.js
+++ src/routes/(home)/[...slug]/+page.server.js
@@ -1,7 +1,6 @@
 import { Pages } from "$lib";
 
 export const load = async ({ locals, params }) => {
-  // this comment is to test update functionality
   return {
     page: await Pages.SHOW(params.slug),
   };
